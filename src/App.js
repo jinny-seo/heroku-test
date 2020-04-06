9ea0f71b9c4a1a93fa8802de2d4cc4c8b92ea358
littledazed <ogum536@gmailcom>
2020-04-06 13:26:39 -0500

    testing commits to both heroku and git

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           whattt
-          testing commits to git
+          testing commits to git and heroku at the same time
         </p>
         <a
           className="App-link"
