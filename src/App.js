1ee7f6992bbb378d71d7c5e49c7a8cbdf9bdecaf
littledazed <ogum536@gmailcom>
2020-04-06 14:18:03 -0500

    attempt 8 at static.json -- added homepage to package.json

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>attempt 7</p>
+        <p>attempt 8</p>
         <a
           className="App-link"
           href="https://reactjs.org"
