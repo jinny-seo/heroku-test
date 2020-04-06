21b62edd08b8690bf2fe95259a9c298de03cf87a
littledazed <ogum536@gmailcom>
2020-04-06 14:46:23 -0500

    omg it worked

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>attempt 8</p>
+        <p>omg it worked</p>
         <a
           className="App-link"
           href="https://reactjs.org"
