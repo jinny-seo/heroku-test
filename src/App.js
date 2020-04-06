fc5db6d1704788d90310459713121f52bf0766fe
littledazed <ogum536@gmailcom>
2020-04-06 13:23:31 -0500

    commit test

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           whattt
+          testing commits to git
         </p>
         <a
           className="App-link"
